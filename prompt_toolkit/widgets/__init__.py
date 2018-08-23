38e23b9b1836558744442fc6415f3afe67d2a1dd
superlevure <laurent.riviere.pro@gmail.com>
2018-08-23 13:58:11 +0200

    add prompt toolkit

---
+++ prompt_toolkit/widgets/__init__.py
@@ -0,0 +1,45 @@
+"""
+Collection of reusable components for building full screen applications.
+These are higher level abstractions on top of the `prompt_toolkit.layout`
+module.
+
+Most of these widgets implement the ``__pt_container__`` method, which makes it
+possible to embed these in the layout like any other container.
+"""
+from __future__ import unicode_literals
+
+from .base import TextArea, Label, Button, Frame, Shadow, Box, VerticalLine, HorizontalLine, RadioList, Checkbox, ProgressBar, SelectableList
+from .dialogs import Dialog
+from .menus import MenuContainer, MenuItem
+from .toolbars import ArgToolbar, CompletionsToolbar, FormattedTextToolbar, SearchToolbar, SystemToolbar, ValidationToolbar
+
+__all__ = [
+    # Base.
+    'TextArea',
+    'Label',
+    'Button',
+    'Frame',
+    'Shadow',
+    'Box',
+    'VerticalLine',
+    'HorizontalLine',
+    'RadioList',
+    'Checkbox',
+    'ProgressBar',
+    'SelectableList',
+
+    # Toolbars.
+    'ArgToolbar',
+    'CompletionsToolbar',
+    'FormattedTextToolbar',
+    'SearchToolbar',
+    'SystemToolbar',
+    'ValidationToolbar',
+
+    # Dialogs.
+    'Dialog',
+
+    # Menus.
+    'MenuContainer',
+    'MenuItem',
+]
