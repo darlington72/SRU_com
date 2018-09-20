e27c79566ad6384b0667bfd0dcf42d63aa630433
superlevure <laurent.riviere.pro@gmail.com>
2018-09-20 10:45:59 +0200

    error handling

--- update.py
+++ update.py
@@ -4,6 +4,7 @@ import tarfile
 from datetime import datetime
 import os
 import shutil
+from socket import gaierror
 from tqdm import tqdm
 import requests
 
@@ -60,97 +61,103 @@ def update():
     print_c("Welcome to SRU_com update.", "info")
     print_c(f"Current version {__version__}, looking for update at {REPO}", "run")
 
-    r = requests.get(REPO, headers={"Accept-Encoding": "identity"})
+    try:
+        r = requests.get(REPO, headers={"Accept-Encoding": "identity"})
 
-    if r.ok:
-        response = json.loads(r.text or r.content)
+        if r.ok:
+            response = json.loads(r.text or r.content)
 
-        last_version_name = response["tag_name"]
-        last_version_url = response["tarball_url"]
+            last_version_name = response["tag_name"]
+            last_version_url = response["tarball_url"]
 
-        if last_version_name > __version__:
-            print_c(
-                f"New version {last_version_name} is available at {last_version_url} !",
-                "good",
-            )
-
-            print_c("Backing up current version to tar file..", "run")
-
-            backup_dir = "backups"
-            if not os.path.exists(backup_dir):
-                os.makedirs(backup_dir)
-            current_date = datetime.now().strftime("%Y-%m-%d")
-            backup_name = "SRU_com-" + __version__ + "-" + current_date + ".tar.gz"
-
-            with tarfile.open(backup_dir + "/" + backup_name, mode="w:gz") as backup:
-                backup.add(
-                    CURRENT_DIR, arcname=os.path.basename(CURRENT_DIR), recursive=True
-                )
-
-            print_c(f"SRU_com has been backed up to {backup_dir}\{backup_name}", "good")
-
-            print_c(f"Downloading new version..", "run")
-
-            r = requests.get(
-                last_version_url, stream=True, headers={"Accept-Encoding": "identity"}
-            )
-
-            if r.status_code == 200:
-                last_version_name += ".tar.gz"
-                with open(last_version_name, "wb") as f:
-                    try:
-                        total_size = r.headers["Content-Length"]
-                    except KeyError:
-                        print_c("Release length unavailable", "info")
-                        total_size = None
-
-                    if total_size is None:
-                        for chunk in r:
-                            f.write(chunk)
-                    else:
-                        with tqdm(
-                            total=100, ncols=100, desc="\033[97m[~]\033[0m "
-                        ) as pbar:
-                            for chunk in r.iter_content(
-                                chunk_size=int(int(total_size) / 99)
-                            ):
-                                f.write(chunk)
-                                pbar.update(1)
-
-                print_c(f"Release downloaded <{last_version_name}>", "good")
-
-                print_c("Decompressing..", "run", False)
-                tar_release = tarfile.open(last_version_name)
-                tar_release.extractall()
-                tar_release_name = tar_release.getnames()[0]
-                print(" Done.")
-
-                print_c("Installing new version..", "run", endline=False)
-                copytree(tar_release_name + "/", CURRENT_DIR + "/")
-                print(" Done.")
-
-                print_c("Deleting temporary files..", "run", endline=False)
-                os.remove(tar_release.name)
-                shutil.rmtree(tar_release_name)
-                print(" Done.")
-
-                print_c(f"New version {last_version_name} has been installed !", "good")
-
-            else:
+            if last_version_name > __version__:
                 print_c(
-                    "Error in HTTP request, please check the connection and try again.",
-                    "bad",
+                    f"New version {last_version_name} is available at {last_version_url} !",
+                    "good",
                 )
+
+                print_c("Backing up current version to tar file..", "run")
+
+                backup_dir = "backups"
+                if not os.path.exists(backup_dir):
+                    os.makedirs(backup_dir)
+                current_date = datetime.now().strftime("%Y-%m-%d")
+                backup_name = "SRU_com-" + __version__ + "-" + current_date + ".tar.gz"
+
+                with tarfile.open(backup_dir + "/" + backup_name, mode="w:gz") as backup:
+                    backup.add(
+                        CURRENT_DIR, arcname=os.path.basename(CURRENT_DIR), recursive=True
+                    )
+
+                print_c(f"SRU_com has been backed up to {backup_dir}\{backup_name}", "good")
+
+                print_c(f"Downloading new version..", "run")
+
+                r = requests.get(
+                    last_version_url, stream=True, headers={"Accept-Encoding": "identity"}
+                )
+
+                if r.status_code == 200:
+                    last_version_name += ".tar.gz"
+                    with open(last_version_name, "wb") as f:
+                        try:
+                            total_size = r.headers["Content-Length"]
+                        except KeyError:
+                            print_c("Release length unavailable", "info")
+                            total_size = None
+
+                        if total_size is None:
+                            for chunk in r:
+                                f.write(chunk)
+                        else:
+                            with tqdm(
+                                total=100, ncols=100, desc="\033[97m[~]\033[0m "
+                            ) as pbar:
+                                for chunk in r.iter_content(
+                                    chunk_size=int(int(total_size) / 99)
+                                ):
+                                    f.write(chunk)
+                                    pbar.update(1)
+
+                    print_c(f"Release downloaded <{last_version_name}>", "good")
+
+                    print_c("Decompressing..", "run", False)
+                    tar_release = tarfile.open(last_version_name)
+                    tar_release.extractall()
+                    tar_release_name = tar_release.getnames()[0]
+                    print(" Done.")
+
+                    print_c("Installing new version..", "run", endline=False)
+                    copytree(tar_release_name + "/", CURRENT_DIR + "/")
+                    print(" Done.")
+
+                    print_c("Deleting temporary files..", "run", endline=False)
+                    os.remove(tar_release.name)
+                    shutil.rmtree(tar_release_name)
+                    print(" Done.")
+
+                    print_c(f"New version {last_version_name} has been installed !", "good")
+
+                else:
+                    print_c(
+                        "Error in HTTP request, please check the connection and try again.",
+                        "bad",
+                    )
+                    sys.exit(0)
+            else:
+                print_c("SRU_com is already up to date", "good")
                 sys.exit(0)
+
         else:
-            print_c("SRU_com is already up to date", "good")
+            print_c(
+                "Error in HTTP request, please check the connection and try again.", "bad"
+            )
             sys.exit(0)
 
-    else:
+    except requests.exceptions.ConnectionError:
         print_c(
-            "Error in HTTP request, please check the connection and try again.", "bad"
+            "Failed to connect, please check the connection and try again.", "bad"
         )
         sys.exit(0)
-
 
 # update()
